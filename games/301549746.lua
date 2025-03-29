ed53ee956473d344ee3839e0506894dc8645cfac
yourafg <elyasnoorzai.afg@gmail.com>
2025-03-29 19:25:59 -0400

    working

---
+++ games/301549746.lua
@@ -0,0 +1,88 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/yourafg/hivality/refs/heads/main/UI/library.lua"))()
+local flags = library.flags
+
+local window = library:window({
+    name = "Hivality", 
+    size = UDim2.fromOffset(600, 500) 
+})
+
+local legit = window:tab({
+    name = "legit"
+})
+local rage = window:tab({
+    name = "rage"
+})
+local esp = window:tab({
+    name = "esp"
+})
+local visuals = window:tab({
+    name = "visuals"
+})
+local misc = window:tab({
+    name = "misc"
+})
+
+local toggle = legit:section({name = "toggle"})
+
+toggle:toggle({
+    name = "Toggle",
+    default = false,
+    callback = function(enabled)
+        if enabled then
+            print("enabled")
+        else
+            print("disabled")
+        end
+    end,
+})
+
+toggle:dropdown({
+    name = "activation", 
+    items = {"mouse 1", "mouse 2", "always"}, 
+    multi = false, 
+    callback = function(option)
+        print(option)
+    end
+})
+
+toggle:slider({
+    suffix = "%", 
+    default = 90, 
+    min = 0, 
+    max = 100, 
+    interval = 0.5,
+    callback = function(num)
+        print(num)
+    end
+})
+
+toggle:colorpicker({
+    name = "color",
+    color = Color3.new(1, 1, 1),
+    callback = function(color, alpha)
+        print(color, alpha)
+    end
+})
+
+local settings = misc:section({name = "UI"})
+
+settings:keybind({
+    name = "UI Bind", 
+    default = Enum.KeyCode.RightShift, 
+    callback = window.set_menu_visibility
+})
+
+settings:toggle({
+    name = "Player List",
+    default = false, 
+    callback = function(bool)
+    window.toggle_playerlist(bool)
+    end
+})
+
+settings:toggle({
+    name = "Watermark", 
+    default = false, 
+    callback = function(bool)
+    window.toggle_watermark(bool)
+end})
--- loader.lua
+++ loader.lua
@@ -1,82 +1,33 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/yourafg/hivality/refs/heads/main/UI/library.lua"))()
-local flags = library.flags
+local hivalityID = game.PlaceId
 
-local window = library:window({
-    name = "Hyori", 
-    size = UDim2.fromOffset(500, 650) 
-})
+if hivalityID == 301549746 then
+    print([[
+          ___ ___ ._______   _________  .____    .___________________.___.
+ /   |   \|   \   \ /   /  _  \ |    |   |   \__    ___/\__  |   |
+/    ~    \   |\   Y   /  /_\  \|    |   |   | |    |    /   |   |
+\    Y    /   | \     /    |    \    |___|   | |    |    \____   |
+ \___|_  /|___|  \___/\____|__  /_______ \___| |____|    / ______|
+       \/                     \/        \/               \/       
 
-local legit = window:tab({
-    name = "legit"
-})
-local rage = window:tab({
-    name = "rage"
-})
-local esp = window:tab({
-    name = "esp"
-})
-local visuals = window:tab({
-    name = "visuals"
-})
-local misc = window:tab({
-    name = "misc"
-})
-
-local toggle = legit:section({name = "toggle"})
-
-toggle:toggle({
-    name = "Toggle",
-    default = false,
-    callback = function(enabled)
-        if enabled then
-            print("enabled")
-        else
-            print("disabled")
-        end
-    end,
-})
-
-toggle:dropdown({
-    name = "activation", 
-    items = {"mouse 1", "mouse 2", "always"}, 
-    multi = false, 
-    callback = function(option)
-        print(option)
-    end
-})
-
-toggle:slider({
-    suffix = "%", 
-    default = 90, 
-    min = 0, 
-    max = 100, 
-    interval = 0.5,
-    callback = function(num)
-        print(num)
-    end
-})
-
-toggle:colorpicker({
-    name = "color",
-    color = Color3.new(1, 1, 1),
-    callback = function(color, alpha)
-        print(color, alpha)
-    end
-})
-
-local settings = misc:section({name = "UI"})
-
-settings:toggle({
-    name = "Watermark", 
-    default = false, 
-    callback = function(bool)
-    window.toggle_watermark(bool)
-end})
-
-settings:toggle({
-    name = "Player List",
-    default = false, 
-    callback = function(bool)
-    window.toggle_playerlist(bool)
-    end
-})
+    ]])
+elseif hivalityID == 17625359962 then
+    print([[
+            ___ ___ ._______   _________  .____    .___________________.___.
+   /   |   \|   \   \ /   /  _  \ |    |   |   \__    ___/\__  |   |
+  /    ~    \   |\   Y   /  /_\  \|    |   |   | |    |    /   |   |
+  \    Y    /   | \     /    |    \    |___|   | |    |    \____   |
+   \___|_  /|___|  \___/\____|__  /_______ \___| |____|    / ______|
+         \/                     \/        \/               \/       
+  
+      ]])
+else
+    warn([[
+     ___ ___ ._______   _________  .____    .___________________.___.
+ /   |   \|   \   \ /   /  _  \ |    |   |   \__    ___/\__  |   |
+/    ~    \   |\   Y   /  /_\  \|    |   |   | |    |    /   |   |
+\    Y    /   | \     /    |    \    |___|   | |    |    \____   |
+ \___|_  /|___|  \___/\____|__  /_______ \___| |____|    / ______|
+       \/                     \/        \/               \/       
+                                                    
+    ]])
+end
